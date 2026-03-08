de614a2c1de9525e1393480ff0812324683719db
sakshi <96122139+sakshi2078@users.noreply.github.com>
2026-03-09 00:40:12 +0530

    Add message for GitHub push trigger

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
     public static void main(String [] args){
         System.out.println("Hello World! This is DevOps!");
         System.out.println("Pipeline works fine!");
+        System.out.println("Trigger by Github Push!");
     }
 }

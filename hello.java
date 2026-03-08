9fe147cc437abd00c5c865bc0de4f5947b46b3a5
sakshi <96122139+sakshi2078@users.noreply.github.com>
2026-03-08 23:06:52 +0530

    Remove unnecessary print statements and add new one

--- hello.java
+++ hello.java
@@ -1,8 +1,7 @@
 public class hello{
     public static void main(String [] args){
         System.out.println("Hello World! This is DevOps!");
-        System.out.println("Yo wassuppp!");
         System.out.println("Jenkins!");
-        System.out.println("Jenkins works!");
+        System.out.println("Poll SCM WORKS!");
     }
 }

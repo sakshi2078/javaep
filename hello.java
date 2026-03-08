86def96a7950f4e0b43487f128ef7f7fee2798ab
sakshi <96122139+sakshi2078@users.noreply.github.com>
2026-03-09 00:18:32 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,6 @@
 public class hello{
     public static void main(String [] args){
         System.out.println("Hello World! This is DevOps!");
-        System.out.println("Jenkins!");
         System.out.println("Pipeline works fine!");
     }
 }

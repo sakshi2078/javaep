8262a4804aa2461eb2767e43aaacaa54040e8257
sakshi <96122139+sakshi2078@users.noreply.github.com>
2026-03-09 00:08:19 +0530

    Clean up output messages in hello.java
    Removed unnecessary print statements related to SCM and WebHooks.

--- hello.java
+++ hello.java
@@ -2,8 +2,6 @@ public class hello{
     public static void main(String [] args){
         System.out.println("Hello World! This is DevOps!");
         System.out.println("Jenkins!");
-        System.out.println("Poll SCM WORKS!");
-        System.out.println("WebHooks works fine!");
         System.out.println("Pipeline works fine!");
     }
 }
